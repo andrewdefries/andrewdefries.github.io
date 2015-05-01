62552c34483e7f8510bd42706f4b1917bde44e25
Andrew Defries PhD <andrew.defries@gmail.com>
2015-05-01 10:03:22 -0700

    Update Master.sh

--- MarkdownGallery/Master.sh
+++ MarkdownGallery/Master.sh
@@ -6,8 +6,14 @@ for i in "${Do[@]}"
 	do
 	name=(`echo $i | sed 's/.sdf//g'`)
 	echo "making $i xyz coords"
-	babel $i -O $name.xyz
-	cat testrgl.Rmd | sed "s/myquery/$name/g" > $name.Rmd
+	babel $i -O $name.xyz --gen3D
+	babel $i -O $name.png
+	cat $name.xyz | sed '1d' | sed '1d' > tmp
+	mv tmp $name.xyz
+#	mywd=(`pwd`)
+#	cat testrgl.Rmd | sed "s/myquery/$name/g" | sed "s/mywd/$mywd/g" > $name.Rmd
+	sdfid=(`head -n 1 Query.sdf`)
+	cat testrgl.Rmd | sed "s/myquery/$name/g" | sed "s/mywd/\/home\/cheminfused\/Desktop\/andrewdefries.github.io\/MarkdownGallery/g" | sed "s/Query/$sdfid/g" > $name.Rmd
 	cat testrgl.R | sed "s/myquery/$name/g" > knit.$name.R
 	echo "kniting $name report"
 	R CMD BATCH knit.$name.R 
